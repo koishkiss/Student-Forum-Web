776e9ec655fed24f5eacd891d38335e442faaf50
koishkiss <202300300074@mail.sdu.edu.cn>
2024-09-25 23:21:27 +0800

    init3

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
-import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+//引入createApp用于创建组件
+import {createApp} from 'vue'
+//引入App根组件
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
