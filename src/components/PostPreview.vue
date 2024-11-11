3dbbdefeff736791ea290c1a3885e5ba371499dc
koishkiss <202300300074@mail.sdu.edu.cn>
2024-11-12 01:17:28 +0800

    明天还要调整

--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -1,7 +1,11 @@
 <script lang="ts">
-  export default {
-    name:'PostPreview',  //组件名
+import { View,ChatLineSquare } from "@element-plus/icons-vue"
+export default {
+  name:'PostPreview',  //组件名
+  components:{
+    View,ChatLineSquare
   }
+}
 </script>
 
 <template>
@@ -15,29 +19,52 @@
       </el-text>
     </div>
 
-    <div class="post-data-box">
-      <el-text>{{ postTime }}</el-text>
-      <el-text>{{ status }}</el-text>
-      <el-text>{{ viewNum }}</el-text>
-    </div>
+    <el-text class="view-num">
+      <el-icon><View /></el-icon>
+      <span>{{ viewNum }}</span>
+    </el-text>
   </div>
 
   <div class="post-content-box">
-    <title class="post-title">
-      {{ title }}
-    </title>
+    <el-text class="post-title">
+      <el-tag type="warning" v-if="status === 1">精华</el-tag>
+      <span class="title">{{ title }}</span>
+    </el-text>
 
-    <el-text class="post-preview-text">
+    <el-text class="post-preview-text" tag="p">
       {{ content }}
     </el-text>
 
-    <el-image :src="coverURL" fit="cover" class="post-cover"/>
+    <div class="post-cover-box">
+      <el-image 
+        :src="coverURL" 
+        fit="cover" 
+        class="post-cover" 
+        lazy 
+        :preview-src-list="[coverURL]" 
+        :initial-index="0"
+      />
+    </div>
   </div>
 
-  <div class="post-data-num-box"> 
-    <el-text>{{ commentNum }}</el-text>
-    <el-text>{{ likeNum }}</el-text>
-    <el-text>{{ bookmarkNum }}</el-text>
+  <el-divider style="margin:5px 0;" />
+
+  <div class="post-data-box"> 
+    <div class="post-data-num-box">
+      <el-text class="data-num-item">
+        <el-icon><ChatLineSquare /></el-icon>
+        <span>{{ commentNum }}</span>
+      </el-text>
+      <el-text class="data-num-item">
+        <el-icon><ChatLineSquare /></el-icon>
+        <span>{{ likeNum }}</span>
+      </el-text>
+      <el-text class="data-num-item">
+        <el-icon><ChatLineSquare /></el-icon>
+        <span>{{ bookmarkNum }}</span>
+      </el-text>
+    </div>
+    <el-text class="post-time">发布于 {{ postTime }}</el-text>
   </div>
 </div>
 </template>
@@ -99,11 +126,77 @@ defineProps([
   font-size: 16px;
 }
 
-.post-data-box {
+.view-num {
   display: flex;
   margin: auto;
   margin-right: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-items: start;
+  font-size: 14px;
+}
+.view-num span {
+  margin-left: 3px;
 }
 
+.post-content-box {
+  margin-top: 10px;
+}
+
+.post-title {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.post-title .title {
+  margin-left: 5px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #000;
+}
+
+.post-preview-text {
+  margin-top: 5px;
+}
+
+.post-cover-box {
+  max-height: 400px;
+  margin-top: 5px;
+  display: flex;
+}
+.post-cover {
+  width: 100%;
+}
+
+.post-data-box {
+  display: flex;
+  flex-direction: row;
+  justify-items: center;
+}
+
+.post-data-num-box {
+  display: flex;
+  flex-direction: row;
+}
+
+.data-num-item {
+  display: flex;
+  margin-left: 40px;
+  margin-right: 30px;
+  flex-direction: row;
+  align-items: center;
+  justify-items: start;
+  font-size: 14px;
+}
+.data-num-item span {
+  margin-left: 3px;
+}
+
+.post-time {
+  margin: auto;
+  margin-right: 8px;
+  font-size: 12px;
+  color: #747474;
+}
 
 </style>
