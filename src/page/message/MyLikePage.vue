aa47ec2e0bb41bce7803de1fa4ebb4d88c39e302
koishikiss <202300300074@mail.sdu.edu.cn>
2024-11-24 17:38:07 +0800

    Update MyLikePage.vue

--- src/page/message/MyLikePage.vue
+++ src/page/message/MyLikePage.vue
@@ -5,8 +5,37 @@ export default {
 </script>
 
 <template>
-<div class="my-like-page-box">
-
+<div v-if="!isFirstLoading" class="my-like-page-box">
+  <ul 
+    v-infinite-scroll="loadMore" 
+    :infinite-scroll-disabled="isLoadingMore || noMore" 
+    class="like-list" 
+  >
+    <li v-for="like in likeList" :key="like.id" class="like-item">
+      <div v-if="like.type === 'comment'">
+        <el-text tag="p" class="like-title">
+          <span class="like-nickname">{{ like.nickname }}</span> 赞了你的评论
+        </el-text>
+        <el-text tag="p" class="like-content">{{ like.content }}</el-text>
+        <el-text tag="p" class="like-time">{{ like.likeTime }}</el-text>
+      </div>
+      <div v-if="like.type === 'post'">
+        <el-text tag="p" class="like-title">
+          <span class="like-nickname">{{ like.nickname }}</span> 赞了你的帖子
+        </el-text>
+        <el-text tag="p" class="like-content">{{ like.content }}</el-text>
+        <el-text tag="p" class="like-time">{{ like.likeTime }}</el-text>
+      </div>
+      <el-divider/>
+    </li>
+  </ul>
+  <div class="tail-control-box">
+    <span v-if="isLoadingMore">加载中...</span>
+    <span v-if="noMore && !isLoadingMore" @click="loadMore">
+      再怎么翻也没有啦!
+      <el-icon class="tail-control-icon"><Refresh /></el-icon>
+    </span>
+  </div>
 </div>
 </template>
 
@@ -105,5 +134,55 @@ onBeforeMount(()=>{
 
 
 <style scoped>
+.like-list {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
 
+.like-item {
+  background-color: #fff;
+  padding: 5px;
+}
+
+.like-title {
+  font-size: 17px;
+}
+.like-nickname {
+  color: #000;
+  font-weight: bold;
+}
+
+.like-content {
+  margin-top: 5px;
+  color: #141414;
+  font-size: 17px;
+}
+
+.like-time {
+  margin-top: 8px;
+  color: #474747;
+  font-size: 12px;
+}
+
+.tail-control-box {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.tail-control-box span {
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 14px;
+  color: grey;
+}
+.tail-control-icon {
+  font-size: 14px;
+  padding-top: 2px;
+  margin-left: 5px;
+}
 </style>
