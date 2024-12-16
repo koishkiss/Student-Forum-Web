2f4afad6ab476336d5f1fe0bf0649c4064c1c7c3
LhyMichael <318543656@qq.com>
2024-12-16 23:09:28 +0800

    板块设置完善

--- src/page/section/SectionSettingPage.vue
+++ src/page/section/SectionSettingPage.vue
@@ -1,6 +1,10 @@
 <script lang="ts">
+import AdminIdentityList from '@/components/AdminIdentityList.vue';
 export default {
-  name:"SectionSettingPage"
+  name:"SectionSettingPage",
+  components:{
+    AdminIdentityList
+  }
 }
 </script>
 
@@ -74,6 +78,41 @@ export default {
     <input type="file" accept=".jpg,.png,.bmp,.jpeg" @change="upload" ref="inputAvatar" hidden>
   </div>
 
+    <div class="section-info-setting-box"  v-if="!isLoadingSection && !isLoadingClassifyList">
+    <el-text tag="p" class="section-info-setting-title">
+      管理员信息设置
+    </el-text>
+    <div class="admin-identity-list-content">
+    <div v-for="(admin,index) in section.adminList" :key="index" class="admin-identity-item"> 
+      <div class="item-box">
+        <div class="avatar-container" 
+          @mouseenter="userInfoCardEnter(index)" 
+          @mouseleave="userInfoCardDelayLeave(index)"
+        >
+          <transition name="user-identity-card-content">
+            <div class="identity-card-container" v-if="showUserIdentityCard[index]">
+              <UserPreviewIdentityCard :theUid="admin.uid"/>
+            </div>
+          </transition>
+          <el-avatar 
+            :src="admin.avatarURL" 
+            shape="square" 
+            :size="90"
+            fit="cover" 
+            class="admin-avatar"
+          />
+        </div>
+
+        <el-text class="admin-name">
+          {{ admin.nickname }}
+          <el-tag :type="admin.identity===1?'success':'warning'" class="identity">
+            {{ admin.identity===1?'管理':'版主' }}
+          </el-tag>
+        </el-text>
+      </div>
+    </div>
+    </div>
+  </div>
 </div>
 </template>
   
@@ -91,6 +130,9 @@ const emits = defineEmits(["update:sectionInfo"])
 
 const { ip_port } = useHttpStore();
 const route = useRoute();
+const hasPerson = computed(()=>{
+  return section.adminList.length === 0 ? false : true;
+})
 
 let section = reactive<SectionInfo>({
   sectionId: -1,
@@ -109,6 +151,8 @@ let section = reactive<SectionInfo>({
 });
 
 let classificationList = reactive([]);
+const showUserIdentityCard = reactive<Array<boolean>>([]);
+var timeId: any[] = [];
 
 const isLoadingSection = ref(true);
 const isLoadingClassifyList = ref(true);
@@ -251,6 +295,20 @@ onBeforeMount(()=>{
   loadClassifyList();
   loadSection();
 })
+
+function userInfoCardEnter(id: number) {
+  if (timeId[id] !== undefined) {
+    clearTimeout(timeId[id]);
+  } else {
+    showUserIdentityCard[id] = true;
+  }
+}
+function userInfoCardDelayLeave(id: number) {
+  timeId[id] = setTimeout(()=>{
+    showUserIdentityCard[id] = false;
+    timeId[id] = undefined;
+  },100)
+}
   
 </script>
   
@@ -275,6 +333,8 @@ onBeforeMount(()=>{
   border-radius: 10px;
   padding: 2px;
   background-color: #f6f9f9;
+  /* display: flex;
+  flex-direction: column; */
 }
 
 .section-info-setting-title {
@@ -326,6 +386,70 @@ onBeforeMount(()=>{
 .item {
   font-size: 16px;
   min-width: 50px;
+}
+
+.admin-identity-list-content {
+  margin-top: 10px;
+  gap: 20px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.item-box {
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.avatar-container {
+  width: 100px;
+  height: 100px;
+}
+.avatar-container:hover .admin-avatar {
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
+  transition: all 0.1s ease-in-out;
+}
+.admin-avatar {
+  border: 2px double #a1a1a1;
+  border-radius: 5px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  width: 90px;
+  height: 90px;
+  transition: all 0.2s ease-in-out;
+}
+.identity-card-container {
+  position: absolute;
+  margin-left: -355px;
+  margin-top: -30px;
+}
+
+.admin-name {
+  display: flex;
+  flex-direction: row;
+  font-size: 13px;
+}
+
+.identity {
+  margin-left: 5px;
+  height: 20px;
+  width: 35px;
+}
+
+
+/* 管理员者身份卡片动画 */
+.user-identity-card-content-enter-active  {
+  transition: opacity 0.3s ease-out, transform 0.3s ease-out;
+}
+.user-identity-card-content-leave-active {
+  transition: opacity 0.3s ease-out, transform 0.2s ease-out;
+}
+.user-identity-card-content-enter-from,
+.user-identity-card-content-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
 }
 
 </style>
